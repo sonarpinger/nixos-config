180e921bbcce8c3982cc2ce353443fd01a9ed78b
sonarpinger <sonarwaver@gmail.com>
2025-08-24 23:22:11 -0700

    add server configs

---
+++ hosts/server/default.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, pkgs-unstable, inputs, username, user, configPath, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/base.nix
+  ];
+
+  networking.hostName = "server";
+  time.timeZone = "America/Los_Angeles";
+
+  users.users.${username}= {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    shell = pkgs.zsh;
+    packages = [ inputs.home-manager.packages.${pkgs.system}.default ];   
+  };
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit
+        inputs
+        username
+        user
+        configPath
+        pkgs-unstable
+        ;
+    };
+    users = {
+      ${username} = import ../../home-manager/default.nix;
+    };
+    backupFileExtension = "backup";
+  };
+  
+  security.sudo.extraConfig = ''
+    %wheel ALL=(ALL) NOPASSWD: ALL
+  '';
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  # Enable all firmware
+  hardware.enableAllFirmware = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.fuse.userAllowOther = true;
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = [ username ];
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  system.stateVersion = "25.05";
+}
